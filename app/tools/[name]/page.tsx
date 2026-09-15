d780cf908147eee62d22f6dd0ff4df6afe4fdd1c
Khan <mahtaballi@gmail.com>
2026-09-15 10:28:54 +0500

    Improve tool page layout

--- app/tools/[name]/page.tsx
+++ app/tools/[name]/page.tsx
@@ -311,7 +311,7 @@ export default async function ToolPage({
               Alternatives to {tool.name}
             </h2>
 
-            <div className="flex flex-wrap gap-3">
+            <div className="grid gap-3 sm:grid-cols-2">
               {tool.alternatives.map((alternative) => (
                 <span
                   key={alternative}
